7953bd009c0a6db6462ff92abf2205278f5c0f32
Lucas Campos <lucas.cgoncalves7@gmail.com>
2023-09-20 13:50:25 -0300

    criação da controller CarrinhoCompraController e criação da viwe Model CarrinhoCompraViewModel

---
+++ LanchesMac/Controllers/CarrinhoCompraController.cs
@@ -0,0 +1,52 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using LanchesMac.Models;
+using LanchesMac.ViewModels;
+
+namespace LanchesMac.Controllers
+{
+    public class CarrinhoCompraController : Controller
+    {
+        private readonly ILancheRepository _lancheRespository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraController(ILancheRepository lancheRespository, CarrinhoCompra carrinhoCompra)
+        {
+            _lancheRespository = lancheRespository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IActionResult Index()
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItem = itens;
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+            return View(carrinhoCompraVM);
+        }
+
+        public IActionResult AdicionarItemNoCarrinhoCompra( int lancheid)
+        {
+            var lancheSelecionado = _lancheRespository.Lanches.FirstOrDefault(p => p.LancheId == lancheid);
+
+            if (lancheSelecionado != null)
+            {
+                _carrinhoCompra.AdicionarAoCarrinho(lancheSelecionado);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult RemoverItemDoCarrinhoCompra(int lancheid)
+        {
+            var lancheSelecionado  = _lancheRespository.Lanches.FirstOrDefault(p => p.LancheId == lancheid);
+            if(lancheSelecionado != null)
+            {
+                _carrinhoCompra.RemoverDoCarrinho(lancheSelecionado);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
